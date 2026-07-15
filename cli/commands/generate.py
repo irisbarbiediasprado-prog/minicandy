a005a864fe451ac6f79067f87361163c41fa0724
Íris Barbie Dias Prado <irisbarbiediasprado@gmail.com>
2026-07-15 14:09:59 -0300

    feat(generate): generate all resources by default

--- cli/commands/generate.py
+++ cli/commands/generate.py
@@ -5,9 +5,17 @@ from cli.generators import theme_resources
 NAME = "generate"
 HELP = "Gera arquivos do icon pack"
 
+def generate_all():
+    drawable.generate()
+    appfilter.generate()
+    theme_resources.generate()
+    print("✅ Geração concluída.")
+
 def register(subparsers):
     parser = subparsers.add_parser(NAME, help=HELP)
-    actions = parser.add_subparsers(dest="generate_command", required=True)
+    actions = parser.add_subparsers(dest="generate_command")
+
+    parser.set_defaults(func=lambda args: generate_all())
 
     p = actions.add_parser("drawable", help="Gera drawable.xml")
     p.set_defaults(func=lambda args: drawable.generate())
